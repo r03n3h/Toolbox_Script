894cb131a89e557b3bf51e93202f5007861649b8
r03n3h <r03n3h@gmail.com>
2022-07-14 22:05:26 +0200

    Add files via upload

---
+++ install_r03n_toolbox.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+sudo -n true
+test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
+
+echo repistory update
+sudo apt update
+
+echo installing the must-have pre-requisites
+while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
+    fuse3
+    open-vm-tools
+    open-vm-tools-desktop
+    ltrace
+    strace
+    flameshot
+    jd-gui
+    ghidra
+    wkhtmltopdf
+    adb
+    apktool
+    dex2jar
+    python3pip
+    tree
+    seclist 
+    feroxbuster
+    mysql
+    jq
+    terminator
+    cupp
+    ripgrep
+    bat
+    exa
+EOF
+)
+
+sudo apt-get install -y tig
+
+echo python apps
+echo -e "\n"
+sleep 1
+
+pip3 install pwntools --no-warn-script-location
+pip3 install apkid
+pip3 install apkleaks
+
+echo variable aanpassen
+echo -e "\n"
+sleep 1
+export PATH=$PATH:/home/kali/.local/bin
+
+
+echo setting up shared folders 
+echo -e "\n"
+sleep 1
+sudo cat <<EOF | sudo tee /usr/local/sbin/mount-shared-folders
+vmware-hgfsclient | while read folder; do
+vmwpath="/mnt/hgfs/\${folder}"
+echo "[i] Mounting \${folder} (\${vmwpath})"
+sudo mkdir -p "\${vmwpath}"
+sudo umount -f "\${vmwpath}" 2>/dev/null
+sudo vmhgfs-fuse -o allow_other -o auto_unmount ".host:/\${folder}" "\${vmwpath}"
+done
+sleep 2s
+EOF
+
+echo setting permissions
+echo -e "\n"
+sleep 2
+sudo chmod +x /usr/local/sbin/mount-shared-folders
+
+echo mounting shared folders
+echo -e "\n"
+sleep 2
+sudo mount-shared-folders
+sudo mkdir ~/shares
+sudo /usr/bin/vmhgfs-fuse .host:/ ~/shares -o subtype=vmhgfs-fuse,allow_other
+
+echo setting up terminal
+echo -e "\n"
+sleep 2
+echo export PATH=$PATH:/home/kali/.local/bin >> ~/.zshrc
+
+echo "alias grep='rg'" >> ~/.zshrc
+echo "alias ls='exa -alg --group-directories-first'" >> ~/.zshrc
+echo "alias cat='batcat'" >> ~/.zshrc
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
+
+source ~/.zshrc
+
+echo Installation complete, happy hacking!
