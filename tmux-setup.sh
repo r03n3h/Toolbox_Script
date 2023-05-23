6878ec7d2c1a7f02ffea512e1907d356f866e9a8
mr_j3r03n <r03n3h@gmail.com>
2023-05-23 05:05:47 -0400

    Create tmux-setup.sh
    Separate setup for tmux terminal

---
+++ tmux-setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+sudo -n true
+test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
+
+echo repistory update
+sudo apt update
+
+echo setting up terminal
+echo -e "\n"
+sleep 2
+echo export PATH=$PATH:/home/kali/.local/bin >> ~/.zshrc
+
+echo "alias grep='rg'" >> ~/.zshrc
+echo "alias ls='exa -alg --group-directories-first'" >> ~/.zshrc
+echo "alias cat='batcat'" >> ~/.zshrc
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
+
+source ~/.zshrc
+
+echo Setting up tmux 
+sleep 1
+cd
+git clone https://github.com/r03n3h/.tmux.git
+sleep 1
+ln -s -f .tmux/.tmux.conf
+sleep 1
+cp .tmux/.tmux.conf.local .
+
+echo Installation complete, happy hacking!
